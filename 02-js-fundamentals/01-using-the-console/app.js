f86ef838cb0621ea4a965461a8ee20a578d8f086
Ethan Yu <sohbmpethan@gmail.com>
2020-07-11 18:00:25 +0900

    01-using-the-console

---
+++ 02-js-fundamentals/01-using-the-console/app.js
@@ -0,0 +1,23 @@
+//
+// Using the console
+//
+// Log to console
+console.log('hello wolrd!');
+console.log(123);
+console.log(true);
+//var greeting = 'Hello';
+//console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('This is an error');
+console.clear();
+console.warn('This is a warning');
+console.time('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.timeEnd('hello')
