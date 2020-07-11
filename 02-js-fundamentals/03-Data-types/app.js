10dd55dce78ca49a9dd8dfd373061fbfd2793fa3
Ethan Yu <sohbmpethan@gmail.com>
2020-07-11 23:58:55 +0900

    03-Data-types

---
+++ 02-js-fundamentals/03-Data-types/app.js
@@ -0,0 +1,52 @@
+//
+// Primitive
+//
+// String
+const name = 'Ethan';
+
+console.log(typeof name);
+
+// Number
+const age = 23;
+
+console.log(typeof age);
+
+// Boolean
+const hasKids = false;
+
+console.log(typeof hasKids);
+
+// Null
+const car = null;
+
+console.log(typeof car);
+
+// Undefined
+let test;
+
+console.log(typeof test);
+
+// Symbol
+const sym = Symbol();
+
+console.log(typeof sym);
+
+//
+// Reference types - Objects
+//
+// Array
+const hobbies = ['movies', 'music'];
+
+console.log(typeof hobbies);
+
+// Object literal
+const address = {
+    city: 'New York City',
+    state: 'NY'
+}
+
+const today = new Date();
+
+console.log(typeof address);
+console.log(typeof today);
+
