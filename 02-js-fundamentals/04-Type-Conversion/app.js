c080f2e6b82b4f2d5f7f2c6ed2eb55707a7f658c
Ethan Yu <sohbmpethan@gmail.com>
2020-07-14 22:02:52 +0900

    04-Type-Conversion

---
+++ 02-js-fundamentals/04-Type-Conversion/app.js
@@ -0,0 +1,35 @@
+let val;
+
+// Number to string
+val = String(555);
+val = String(4 + 44);
+
+// Bool to string
+val = String(true)
+
+// Date to string
+val = String(new Date());
+
+// Array to string
+val = String([1,2,3,4]);
+
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello'); // NaN
+val = Number([1,2,3]); // NaN
+
+val = parseInt('100.30');
+val = parseFloat('100.30');
+
+// Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed());
