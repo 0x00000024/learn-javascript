c0d1eb004d51fe5ec828d70273f56a45325d09d9
Ethan Yu <sohbmpethan@gmail.com>
2020-07-16 22:08:12 +0900

    05-Numbers-Math-Object

---
+++ 02-js-fundamentals/05-Numbers-Math-Object/app.js
@@ -0,0 +1,25 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(9);
+val = Math.abs(-3);
+val = Math.pow(4, 2);
+val = Math.min(2, 3, 54, 423, 43, 43, 1, 2);
+val = Math.max(2, 3, 54, 423, 43, 43, 1, 2);
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
