2f53d322950879b5ba7d8136ea23def55eed6d03
Ethan Yu <sohbmpethan@gmail.com>
2020-07-20 13:01:13 +0900

    02-DOM-Selectors-Single-Elements

--- 03-DOM-Manipulation-Events/02-DOM-Selectors-Single-Elements/app.js
+++ 03-DOM-Manipulation-Events/02-DOM-Selectors-Single-Elements/app.js
@@ -20,3 +20,16 @@ document.getElementById('task-title').innerText = 'My Tasks';
 document.getElementById('task-title').innerHTML = '<span style="color: red">Task List</span>';
 
 // document.querySelector()
+// only select first element
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'red';
+document.querySelector('li:nth-child(4)').textContent= 'hi';
+document.querySelector('li:nth-child(odd)').style.background = '#dcdbdb';
+document.querySelector('li:nth-child(even)').style.background = '#dbdbdb';
