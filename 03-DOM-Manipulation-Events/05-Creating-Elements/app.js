6ea624beb17e4f613d759e1ad916b3077eeb20bc
Ethan Yu <sohbmpethan@gmail.com>
2020-08-29 21:38:28 +0900

    05-Creating-Elements

--- 03-DOM-Manipulation-Events/05-Creating-Elements/app.js
+++ 03-DOM-Manipulation-Events/05-Creating-Elements/app.js
@@ -13,6 +13,19 @@ li.setAttribute('title', 'New Item');
 // Create text node and append
 li.appendChild(document.createTextNode('Hello World'));
 
+// Create new link element
+const link = document.createElement('a');
+
+// Add classes
+link.className = 'delete-item secondary-content';
+
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove">';
+
+// Append link into li
+li.appendChild(link);
+
 // Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
 
 console.log(li);
