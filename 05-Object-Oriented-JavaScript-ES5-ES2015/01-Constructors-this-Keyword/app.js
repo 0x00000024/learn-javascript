37ef31fab2f0649545d5e7419bcbf79f9db4829c
Ethan Yu <sohbmpethan@gmail.com>
2020-09-15 18:40:40 +0900

    01-Constructors-this-Keyword

---
+++ 05-Object-Oriented-JavaScript-ES5-ES2015/01-Constructors-this-Keyword/app.js
@@ -0,0 +1,27 @@
+// const brad = {
+//     name: 'Brad',
+//     age: 30
+// }
+//
+// console.log(brad);
+// console.log(brad.age);
+
+// Person constructor
+function Person(name, dob) {
+    this.name = name;
+    // this.age = age;
+    this.birthday = new Date(dob);
+    this.calculateAge = function () {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+}
+
+// const brad = new Person('Brad', 36);
+// const john = new Person('John', 30);
+
+// console.log(john.age);
+
+const brad = new Person('Brad', '9-10-1981');
+console.log(brad.calculateAge());
