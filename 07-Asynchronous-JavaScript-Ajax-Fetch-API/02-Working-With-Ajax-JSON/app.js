e43101b1cd57a3a7da1c57cf81daf5170d398614
Ethan Yu <sohbmpethan@gmail.com>
2020-10-04 00:57:28 +0900

    02-Working-With-Ajax-JSON

---
+++ 07-Asynchronous-JavaScript-Ajax-Fetch-API/02-Working-With-Ajax-JSON/app.js
@@ -0,0 +1,57 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load Single Customer
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json');
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  };
+
+  xhr.send();
+}
+
+// Load Customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json');
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer) {
+        output += `
+          <ul>
+            <li>ID: ${customer.id}</li>
+            <li>Name: ${customer.name}</li>
+            <li>Company: ${customer.company}</li>
+            <li>Phone: ${customer.phone}</li>
+          </ul>
+        `;
+      });
+      document.getElementById('customers').innerHTML = output;
+    }
+  };
+
+  xhr.send();
+}
