a70b6eb233f4839fae3567e50a7840f271a5c8ba
Ethan Yu <sohbmpethan@gmail.com>
2020-10-04 18:27:42 +0900

    09-Arrow-Functions

---
+++ 07-Asynchronous-JavaScript-Ajax-Fetch-API/09-Arrow-Functions/app.js
@@ -0,0 +1,38 @@
+// const sayHello = function() {
+//   console.log('Hello');
+// };
+
+// const sayHello = () => {
+//   console.log('Hello');
+// };
+
+// One line function does not need braces
+// const sayHello = () => console.log('Hello');
+
+// One line returns
+// const sayHello = () => 'Hello';
+
+// Same as above
+// const sayHello = function() {
+//   return 'Hello';
+// };
+
+// Return object
+// const sayHello = () => ({msg: 'Hello'});
+
+// Single param does not need parenthesis
+// const sayHello = (name) => console.log(`Hello ${name}`);
+
+// Multiple params need parenthesis
+// const sayHello = (firstName, lastName) => console
+//     .log(`Hello ${firstName} ${lastName}`);
+
+// sayHello('Brad', 'Traversy');
+
+const users = ['Nathan', 'John', 'William'];
+
+const nameLengths = users.map(function(name) {
+  return name.length;
+});
+
+console.log(nameLengths);
