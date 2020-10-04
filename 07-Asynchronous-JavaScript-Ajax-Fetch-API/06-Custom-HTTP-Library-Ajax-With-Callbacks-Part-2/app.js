490918a383b5d1741fd2d3132188f85dc0d3eb40
Ethan Yu <sohbmpethan@gmail.com>
2020-10-04 17:03:28 +0900

    06-Custom-HTTP-Library-Ajax-With-Callbacks-Part-2

---
+++ 07-Asynchronous-JavaScript-Ajax-Fetch-API/06-Custom-HTTP-Library-Ajax-With-Callbacks-Part-2/app.js
@@ -0,0 +1,43 @@
+const http = new EasyHTTP;
+
+// GetPosts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Create Data
+// const data = {
+//   title: 'Custom Post',
+//   body: 'This is a custom post',
+// };
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
