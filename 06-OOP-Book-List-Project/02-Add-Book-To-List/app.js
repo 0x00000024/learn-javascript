8398bb59430ed3f4b3c6730c8e7433b85aeaef51
Ethan Yu <sohbmpethan@gmail.com>
2021-03-17 16:30:47 +0900

    02-Add-Book-To-List

---
+++ 06-OOP-Book-List-Project/02-Add-Book-To-List/app.js
@@ -0,0 +1,54 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {}
+
+// Add book to list
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById('book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete"></a></td>
+  `;
+  list.append(row);
+};
+
+// Clear fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+};
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit',
+    function(e) {
+      // Get form values
+      const title = document.getElementById('title').value;
+      const author = document.getElementById('author').value;
+      const isbn = document.getElementById('isbn').value;
+
+      // Instantiate book
+      const book = new Book(title, author, isbn);
+
+      // Instantiate UI
+      const ui = new UI();
+
+      // Add book to list
+      ui.addBookToList(book);
+
+      // Clear fields
+      ui.clearFields();
+
+      e.preventDefault();
+    });
